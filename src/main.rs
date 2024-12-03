27e5d8ba55370cadc777f11052b2e9d8e004487a
zimrustpro <zimrustpro@gmail.com>
2024-12-03 11:18:45 +0200

    Implement `SaysHello` trait for every other type T

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,17 @@ trait SaysHello {
     }
 }
 
+impl<T> SaysHello for T {
+    
+}
+
+struct Nothing;
+
 fn main() {
-    println!("Hello, world!");
+    8.hello();
+    &'c'.hello();
+    &mut String::from("Hello there").hello();
+    8.7897.hello();
+    Nothing.hello();
+    std::collections::HashMap::<i32, i32>::new().hello();
 }
