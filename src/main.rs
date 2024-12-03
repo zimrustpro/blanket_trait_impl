23daeaa91686a52e1698d2b29f6a1f460473ca3b
zimrustpro <zimrustpro@gmail.com>
2024-12-03 11:28:35 +0200

    Blanket impl of trait for anything that implements `std::error::Error`

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,43 @@
-trait SaysHello {
-    fn hello(&self) {
-        println!("Hello");
+use std::error::Error as StdError;
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+enum SystemError {
+    #[error("Couldn't send: {0}")]
+    SendError(i32),
+
+    #[error("External crate error: {0}")]
+    ExternalCrateError(String),
+}
+
+trait ToSystemError<T> {
+    fn to_system_error(self) -> Result<T, SystemError>;
+}
+
+impl<T,E: StdError> ToSystemError<T> for Result<T, E> {
+    fn to_system_error(self) -> Result<T, SystemError> {
+        self.map_err(|e| SystemError::ExternalCrateError(e.to_string()))
     }
 }
 
-impl<T> SaysHello for T {
-    
+fn parse_then_send(input: &[u8]) -> Result<(), SystemError> {
+    let some_str = std::str::from_utf8(input).to_system_error()?;
+    let number = some_str.parse::<i32>().to_system_error()?;
+    Ok(())
 }
 
-struct Nothing;
+fn send_number(number: i32) -> Result<(), SystemError> {
+    if number < 1_000_000 {
+        println!("Number sent!");
+        Ok(())
+    } else {
+        println!("Too large!");
+        Err(SystemError::SendError(number))
+    }
+}
 
 fn main() {
-    8.hello();
-    &'c'.hello();
-    &mut String::from("Hello there").hello();
-    8.7897.hello();
-    Nothing.hello();
-    std::collections::HashMap::<i32, i32>::new().hello();
+   println!("{}", parse_then_send(b"nine").unwrap_err());
+   println!("{:?}", parse_then_send(b"nine"));
+   println!("{:?}", parse_then_send(b"10"));
 }
